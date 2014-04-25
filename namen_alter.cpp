6c3290cf8431254ce62ad032171a6f85517e13c8
Sergej Gerk <=>
2014-04-26 00:36:55 +0200

    Alter in Monaten ausgibt

--- namen_alter.cpp
+++ namen_alter.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	cout<< "Bitte geben Sie Ihren Vornamen und Ihr Alter ein\n";
 	string first_name = "???";
-	int age = -1;
+	double age = -1;
 	cin >> first_name >> age;
-	cout << "Hallo " << first_name << " (Alter " << age << ") \n";
+	cout << "Hallo " << first_name << " (Alter " << age*12 << ") \n"; //age in Monaten
 }
