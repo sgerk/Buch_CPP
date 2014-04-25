8360cdb9e0e61133771606de207fadafe0274c77
Sergej Gerk <=>
2014-04-26 00:23:51 +0200

    Hello World

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+int main()
+{
+	cout << "Hallo Programmierer!\nEs geht los!\n";
+	//keep_window_open(); //wartet auf die Eingabe eines Zeichens
+	return 0;
+}
