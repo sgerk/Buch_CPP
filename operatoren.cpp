31eb8a4e0efb5c51e123deb18aaef7196d6d7150
Sergej Gerk <=>
2014-04-26 01:37:28 +0200

    Operatoren-Test

--- operatoren.cpp
+++ operatoren.cpp
@@ -8,14 +8,8 @@ using namespace std;
 int main()
 {
 	cout << "Geben Sie einen Gleitkomaert ein: ";
-	double n;
-	cin >> n;
-	cout << "n ==" << n
-		<< "\nn+1 == " << n+1
-		<<"\ndreimal n == " << 3*n
-		<< "\nzweimal n == " << n+n
-		<< "\nn zum Quadrat == " << n*n
-		<< "\nHaelfte von n == " << n/2
-		<< "\nWurzel von n == " << sqrt(n)
-		<< endl;
+	int a;
+	int b;
+	cin >> a >> b;
+	cout << (a/b*b + a%b == a) << "\n"; //Ausgabe gibt true "1" aus
 }
